537074c58e1e6167a84eebb40bb766653c78dc08
Gulay Movlamova (IT) <gmovlamova@azercell.com>
2026-05-08 16:08:27 +0400

    added: sideBar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Food, Products } from './services/food';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Sidebar } from "./layouts/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/layouts/sidebar/sidebar.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+  activeLink = signal<string>('home');
+}
