d488a047e01eef32f3be7ee5c45889f1eabe2df0
Gulay Movlamova (IT) <gmovlamova@azercell.com>
2026-05-13 14:50:50 +0400

    feat: search logic

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Food, Products } from '../../services/food';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Orders } from "./components/orders/orders";
+import { Orders } from './components/orders/orders';
 
 @Component({
   selector: 'app-home-page',
@@ -14,10 +14,27 @@ export class HomePage {
   today: number = Date.now();
   activeCat = signal<string>('hot dishes');
   data = signal<Products>([]);
+  searchedItem = signal<string>('');
   private foodService = inject(Food);
 
-  constructor(){
-    this.foodService.getProducts().pipe(takeUntilDestroyed()).subscribe(response => this.data.set(response));
+  constructor() {
+    this.foodService
+      .getProducts()
+      .pipe(takeUntilDestroyed())
+      .subscribe((response) => this.data.set(response));
     console.log(this.data());
+  }
+
+  filteredData = computed(() => {
+    return this.data().filter((item) =>{
+      const matchesCategory = item.category.trim() === this.activeCat().trim();
+      const matchesSearch = this.searchedItem() === '' || 
+        item.name.trim().toLowerCase().includes(this.searchedItem());
+      return matchesCategory && matchesSearch;
+    });
+  });
+
+  searchData(event: Event) {
+    this.searchedItem.set((event.target as HTMLInputElement).value.trim().toLowerCase());
   }
 }
