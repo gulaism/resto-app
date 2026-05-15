2d6de3f1a939f2acbb1e56fc1a3aca679b6d7651
Gulay Movlamova (IT) <gmovlamova@azercell.com>
2026-05-15 12:14:39 +0400

    feat: payment first half

--- src/app/pages/home-page/components/payment/payment.ts
+++ src/app/pages/home-page/components/payment/payment.ts
@@ -1,11 +1,14 @@
 import { Component, output, signal } from '@angular/core';
 import { Orders } from "../orders/orders";
+import { Input } from "../../../../shared/input/input";
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.html',
   styleUrl: './payment.css',
+  imports: [Input],
 })
 export class Payment {
   goBack = output<void>();
+  chosenMethod = signal<string>('creditCard');
 }
--- src/app/shared/input/input.ts
+++ src/app/shared/input/input.ts
@@ -7,6 +7,38 @@ import { Component, input } from '@angular/core';
   styleUrl: './input.css',
 })
 export class Input {
+  maxLength = input<number>(500);
+  inputType = input<string>('text');
   placeholder = input<string>('');
   value = input<string>('');
+  isCardInput = input<boolean>(false);
+  isExpiryInput = input<boolean>(false);
+  isCvvInput = input<boolean>(false);
+
+  formatCardNumber(event: Event) {
+    if(!this.isCardInput()) return;
+    let value = (event.target as HTMLInputElement).value;
+    value = value.replace(/\s/g, '').replace(/\D/g, '');
+    value = value.match(/.{1,4}/g)?.join(' ') ?? value;
+    (event.target as HTMLInputElement).value = value;
+  }
+
+  formatExpiry(event: Event) {
+    if(!this.isExpiryInput()) return;
+    let value = (event.target as HTMLInputElement).value;
+    value = value.replace(/\D/g, '');
+    if(value.length >= 2) {
+      value = value.slice(0, 2) + '/' + value.slice(2, 4);
+    }
+    (event.target as HTMLInputElement).value = value;
+  }
+
+  formatCvv(event: Event) {
+    if(!this.isCvvInput()) return;
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, '');
+    value = value.slice(0, 3);
+    input.value = value;
+    
+  }
 }
